e2354a8c93cd4a2e78fe508e34e55d936fd3c5e3
Rilder Almeida <rilder.almeida@arquivei.com.br>
2022-03-07 12:01:59 -0300

    deu certo

--- json_player_store.go
+++ json_player_store.go
@@ -1,59 +1,46 @@
 package main
 
-import "fmt"
-
 const (
 	JSONFILENAME = "players.json"
 )
 
 type Player struct {
-	name  string
-	score int
+	Name  string `json:"name"`
+	Score int    `json:"score"`
 }
 
 type JsonPlayerStore struct {
-	store []*Player
-
-	// ! WHY GOD? WHY?????
-	// ? store []*Player != store []Player
-	// ? &JsonPlayerStore{[]*Player{}} != &JsonPlayerStore{[]Player{}}
-	// ? &JsonPlayerStore{*[]Player{}} != &JsonPlayerStore{[]*Player{}}
+	Store []Player `json:"store"`
 }
 
 func NewJsonPlayerStore() *JsonPlayerStore {
-	return &JsonPlayerStore{[]*Player{}}
+	return &JsonPlayerStore{}
 }
 
 func (j *JsonPlayerStore) RecordWin(name string) {
-	err := ReadJsonFromFile(JSONFILENAME, &j.store)
+	err := FromJsonFile(j)
 	if err != nil {
-		fmt.Println("Error reading to file: ", err)
+		panic(err)
 	}
-	for _, p := range j.store {
-		if p.name == name {
-			p.score++
-			err := WriteJsonToFile(JSONFILENAME, &j.store)
-			if err != nil {
-				fmt.Println("Error writing to file: ", err)
-			}
+	for i, p := range j.Store {
+		if p.Name == name {
+			j.Store[i].Score++
+			ToJsonFile(j)
 			return
 		}
 	}
-	j.store = append(j.store, &Player{name, 1})
-	err = WriteJsonToFile(JSONFILENAME, &j.store)
-	if err != nil {
-		fmt.Println("Error writing to file: ", err)
-	}
+	j.Store = append(j.Store, Player{name, 1})
+	ToJsonFile(j)
 }
 
 func (j *JsonPlayerStore) GetPlayerScore(name string) int {
-	err := ReadJsonFromFile(JSONFILENAME, &j.store)
+	err := FromJsonFile(j)
 	if err != nil {
-		fmt.Println("Error reading to file: ", err)
+		panic(err)
 	}
-	for _, p := range j.store {
-		if p.name == name {
-			return p.score
+	for i, p := range j.Store {
+		if p.Name == name {
+			return j.Store[i].Score
 		}
 	}
 	return 0
