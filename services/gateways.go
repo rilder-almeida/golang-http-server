daabed54193cce6fccd137ba559e0d2758b5e818
Rilder Almeida <rilder.almeida@arquivei.com.br>
2022-04-19 14:18:15 -0300

    refactoring

--- services/gateways.go
+++ services/gateways.go
@@ -1,27 +1,10 @@
 package services
 
-import (
-	"github.com/golang-http-server/entities"
-)
-
-const (
-	POST_RESPONSE_CONTENT_TYPE = "application/json"
-	JSON_FILENAME              = "store.json"
-)
-
 type NfeStore interface {
-	PostRequestReceiver(JsonPostRequest) (JsonResponse, error)
-	GetRequestReceiver(JsonGetRequest) (JsonResponse, error)
-	RequestResponder(string, []byte, int) JsonResponse
-	//utils
-	AssertIdIsNew(string) bool
-	StoreNfe(JsonPostRequest) ([]byte, error)
-	GetNfeById(string) ([]byte, error)
-	MakeJsonNfeIsNew(bool) []byte
+	Receiver(Request) (*[]byte, error)
+	Responder([]byte) (Response, error)
 }
 
-type JsonResponse struct {
-	contentType string
-	bodyData    []byte
-	httpStatus  int
-}
+type Request interface{}
+
+type Response interface{}
